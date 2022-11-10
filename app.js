47a8caa187dd6adca796c365462738888bea10ff
carmencampillo <carmencs12@hotmail.com>
2022-11-10 19:23:18 +0100

    Test

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ const express = require("express");
 const ejs = require("ejs");
 
 const app = express();
+// Test
 
 // ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
 require("./config")(app);
